0db8760612a2f5e88df3fdcfdcef0035d4ca619a
José Miguel Toribio Castro <josemi@macbook-pro-de-jose.home>
2021-10-11 21:30:05 +0200

    Módulo 01 entregado

---
+++ Módulo 01/js/demo.js
@@ -0,0 +1,7 @@
+document.getElementById("name").value = "José Miguel";
+
+document.getElementById("lastname").value = "Toribio Castro";
+
+document.getElementById("usuario").value = "josemitoribio";
+
+document.getElementById("avatar").src = "img/avatar.jpg"
