dda0c525876cd523992bdebfcea85e11722cbbaf
josemitoribio <91979839+josemitoribio@users.noreply.github.com>
2023-03-30 19:43:26 +0200

    Delete Seminario 01 directory

--- Seminario 01/js/demo.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("name").value = "José Miguel";
-
-document.getElementById("lastname").value = "Toribio Castro";
-
-document.getElementById("usuario").value = "josemitoribio";
-
-document.getElementById("avatar").src = "img/avatar.jpg"
